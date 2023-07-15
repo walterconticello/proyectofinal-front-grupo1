ae26c4cd5392f7e4cc7b96d7487fb557f57e9f08
Walter Conticello <walterconticellomsmt@gmail.com>
2023-07-14 21:31:46 -0300

    instalo y configuro paquetes y dependencias

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
-import { useState } from 'react'
+import react from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = react.useState(0)
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
+        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
+        <a href="https://react.dev" target="_blank" rel="noreferrer">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
