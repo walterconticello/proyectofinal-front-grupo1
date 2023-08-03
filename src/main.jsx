965f1938744cef3283b2e9048ba952b85ea51266
Walter Conticello <walterconticellomsmt@gmail.com>
2023-08-03 11:50:58 -0300

    preparacion de librerias necesarias para trabajar en el Layout y creacion basica de los componentes

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client'
 import "../node_modules/bootstrap/dist/css/bootstrap.css"
 import App from './App.jsx'
 import './index.css'
+// eslint-disable-next-line no-unused-vars
+import * as bootstrap from 'bootstrap'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
