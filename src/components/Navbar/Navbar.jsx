67dd9dafdd9dd872cc03064b43ecac9ae7ecfa7b
Walter Conticello <walterconticellomsmt@gmail.com>
2023-08-03 23:14:42 -0300

    fixeando colores y animacion en navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -39,7 +39,7 @@ const Navbar = () => {
 							<div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
 								<div className="flex flex-shrink-0 items-center">
 									<img
-										className="h-8 w-auto"
+										className="h-10 w-auto"
 										src={logoApp}
 										alt="LogoReservaGol"
 									/>
