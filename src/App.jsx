399be5feac7854487099222ae1c6bb638e1dcb9b
Diego Vaca Paz <diegovacapaz@gmail.com>
2023-08-08 16:30:59 -0300

    Se hizo la primera parte del detalle y su css

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,20 @@ import SportCenterDetail from './components/SportCenter/SportCenterDetail';
 
 function App() {
 
+  const sportCenter = {
+    _id: "ObjectId(sportCenter)",
+    name: "La Morocha - Fútbol",
+    address: "Av. Perón 1226",
+    location: `26°48'12.8"S 65°16'44.6"W`,
+    photo: "https://imgar.zonapropcdn.com/avisos/1/00/44/29/27/76/720x532/1724306414.jpg",
+    owner: "ObjectId(owner)",
+    isActive: true,
+  }
 
   return (
     <>
       <Navbar />
-      <SportCenterDetail></SportCenterDetail>
+      <SportCenterDetail sportCenter={sportCenter}></SportCenterDetail>
       <Footer />
     </>
   );
--- src/components/SportCenter/SportCenterDetail.jsx
+++ src/components/SportCenter/SportCenterDetail.jsx
@@ -1,6 +1,31 @@
+import "./SportCenterDetail.css";
+
 const SportCenterDetail = ({sportCenter}) => {
+    /*
+      const sportCenter = {
+    _id: "ObjectId(sportCenter)",
+    name: "Sport Center",
+    address: "Av. Perón 1226",
+    location: `26°48'12.8"S 65°16'44.6"W`,
+    photo: "",
+    owner: "ObjectId(owner)",
+    isActive: true,
+  }*/
+    
     return (
-        <div>Detail</div>
+        <main className="container">
+            <h1 className="text-center fs-1 text-green my-4">{sportCenter.name}</h1>
+            <section className="d-flex gap-5 flex-column flex-md-row align-items-center">
+                <article className="main-picture rounded-3">
+                    {/* <img src={sportCenter.photo} alt={`${sportCenter.name} photo`} /> */}
+                    <div className="bg-primary h-100 w-100"></div>
+                </article>
+                <article>
+                    <p>Dirección: {sportCenter.address}</p>
+                </article>
+            </section>
+            <section></section>
+        </main>
     );
 }
 
