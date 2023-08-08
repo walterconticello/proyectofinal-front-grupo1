6120746eccb5a1c0980672560794269adc0dd5bf
Diego Vaca Paz <diegovacapaz@gmail.com>
2023-08-08 18:50:18 -0300

    Se hizo el marcador en el mapa

--- src/components/SportCenter/Map/MapView.jsx
+++ src/components/SportCenter/Map/MapView.jsx
@@ -1,11 +1,15 @@
-import { MapContainer, TileLayer } from "react-leaflet";
+import { MapContainer, Marker, TileLayer } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
+import MarkerIcon from "./MarkerIcon";
 
 const MapView = ({latitude, longitude}) => {
     return (
-        <MapContainer center={{lat: latitude, lng: longitude}} zoom={10}>
-            <TileLayer attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors' url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-        </MapContainer>
+        <>
+            <MapContainer center={{lat: latitude, lng: longitude}} zoom={14}>
+                <TileLayer attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors' url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+                <Marker position={{lat: latitude, lng: longitude}} icon={MarkerIcon}/>
+            </MapContainer>
+        </>
     );
 }
 
