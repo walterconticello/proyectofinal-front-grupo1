c89bd0718644d0aab14b2ec9705ab30ea5abb127
Pablo Matias Rodriguez <Pablomatiasrodriguez95@gmail.com>
2023-08-10 20:11:51 -0300

    Arreglando Cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import CardList from './components/CardList/CardList';
 import Footer from './components/Footer/Footer';
 import Navbar from './components/Navbar/Navbar';
 import SearchBar from './components/SearchBar/SearchBar';
+import CardList from './components/Card/CardList';
 
 function App() {
   const cards = [
--- src/components/Card/CardList.jsx
+++ src/components/Card/CardList.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import Card from './Card';
+import Card from './card';
+
 
 function CardList(props) {
   return (
