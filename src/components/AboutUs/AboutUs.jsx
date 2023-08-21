a2efd888a4a254e4fcaece3a5d945e538aabca2f
Pablo Matias Rodriguez <Pablomatiasrodriguez95@gmail.com>
2023-08-21 16:37:01 -0300

    Creando una base de About Us

---
+++ src/components/AboutUs/AboutUs.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const AboutUs = () => {
+  return (
+    <div>
+      <h2>Sobre nosotros</h2>
+      <p>Somos una empresa dedicada a ofrecer soluciones innovadoras en el campo de la tecnología. Nuestra misión es ayudar a nuestros clientes a alcanzar sus objetivos de negocio a través de soluciones personalizadas y de alta calidad.</p>
+      <p>Nuestro equipo está formado por expertos en tecnología con años de experiencia en el campo. Nos apasiona lo que hacemos y nos esforzamos por ofrecer el mejor servicio posible a nuestros clientes.</p>
+      <img src="images/about-us.jpg" alt="Acerca de nosotros" className="img-fluid" />
+      <a href="#" className="btn btn-primary">Conoce más sobre nosotros</a>
+    </div>
+  );
+};
+
+export default AboutUs;
