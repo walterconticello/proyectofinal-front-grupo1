57cacaaa3ca9d47f9b7e1100725b56e7a5391dcd
Pablo Matias Rodriguez <Pablomatiasrodriguez95@gmail.com>
2023-08-21 11:36:58 -0300

    Modificando promos de Carousel con Messi

--- src/components/carouselPromo/carouselPromo.jsx
+++ src/components/carouselPromo/carouselPromo.jsx
@@ -1,6 +1,6 @@
 import Carousel from 'react-bootstrap/Carousel';
 import { Link } from 'react-router-dom';
-import sliderImage1 from './imgCarouselPromo/slider1.jpg';
+import sliderImage1 from './imgCarouselPromo/slider1.jpeg';
 import sliderImage2 from './imgCarouselPromo/slider2.jpg';
 import sliderImage3 from './imgCarouselPromo/slider3.jpg';
 
