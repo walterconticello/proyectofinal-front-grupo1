6c59cbe312900e814b3498911adf09172b104c3e
Pablo Matias Rodriguez <Pablomatiasrodriguez95@gmail.com>
2023-08-20 21:31:38 -0300

    Modificando Card para que tome las caracteristicas desde back

--- src/components/cardSportCenter/CardSportCenter.jsx
+++ src/components/cardSportCenter/CardSportCenter.jsx
@@ -1,18 +1,35 @@
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
 import './CardSportCenter.css';
+import { Link } from 'react-router-dom';
+import { cardContext } from '../../context/cardContext';
+import { useContext, useState , useEffect } from 'react';
+import {Card, Button } from 'react-bootstrap';
 
-function CardSportCenter() {
-  return (
-    <Card className="card-sport-center">
-      <Card.Body>
-        <Card.Title>SportCenter</Card.Title>
-        <Button>Alquilar</Button>
-      </Card.Body>
-    </Card>
-    
-  );
-}
+const CardSportCenter = () => {
+    const {Cards} = useContext(cardContext);
+    const [cards, setCards] = useState([]);
+    const [selectedCard, setSelectedCard] = useState(null);
 
+    const Card = ({ card }) => {
+      return (
+        <Card className="h-100">
+          <div className="card-image-container">
+            <Card.Img src={card.image.url} alt={card.name} className="card-image img-fluid" />
+          </div>
+          <Card.Body>
+            <Card.Title>{card.name}</Card.Title>
+            <Card.Text>{card.description}</Card.Text>
+            <div className="card-buttons d-flex justify-content-between">
+              <Button variant="primary" className="buy-button btn">
+                Alquilar
+              </Button>
+              <Button variant="secondary" className="details-button btn">
+                Details
+              </Button>
+            </div>
+          </Card.Body>
+        </Card>
+      );
+    };
+  };
 
 export default CardSportCenter;
