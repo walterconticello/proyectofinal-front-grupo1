9615e974a73c13781379478d363e36b5554bb1ab
Diego Vaca Paz <diegovacapaz@gmail.com>
2023-08-21 19:12:17 -0300

    Se hizo validaciones al date picker

--- src/components/SportCenter/Reservation.jsx
+++ src/components/SportCenter/Reservation.jsx
@@ -2,11 +2,16 @@ import { useEffect, useState } from "react";
 import { Button, Form, Modal } from "react-bootstrap";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import clsx from "clsx";
 
 const Reservation = ({show, onHide, field}) => {
     
     const [startDate, setStartDate] = useState(null);
     const [reservations, setReservations] = useState([]);
+    const [errorFecha, setErrorFecha] = useState({error: false,message: ""});
+    const [clicked, setClicked] = useState(false);
 
     const user = {
         username: "diego_vacapaz",
@@ -24,7 +29,7 @@ const Reservation = ({show, onHide, field}) => {
     const endDate = new Date(new Date().getFullYear(), addMonth(new Date().getMonth(), 2), new Date().getDate());
     const startTime = new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDate(),field.openHour);
     const endTime = new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDate(),field.closeHour - 1);
-
+    
     const fetchReservas = async () => { //Esto deberia filtrar por id de field, pero el dbjson no puedo por eso lo filtro en el try-catch
         try {
             const response = await fetch(`${URL}reservations`);
@@ -33,10 +38,9 @@ const Reservation = ({show, onHide, field}) => {
             setReservations(fieldReservations);
         }
         catch (error) {
-            console.log(error)
+            console.log(error);
         }
     }
-
     const filterReservedHours = () => {
 
     }
@@ -44,6 +48,50 @@ const Reservation = ({show, onHide, field}) => {
     useEffect(() => {
         fetchReservas();
     },[]);
+
+    const validacionFecha = () => {
+        if(!startDate) {
+            return {
+                error: true,
+                message: "Requerido"
+            };
+        }
+
+        if(startDate.getFullYear < startTime.getFullYear() ||(startDate.getFullYear < startTime.getFullYear() && startDate.getMonth() < startTime.getMonth()) || (startDate.getFullYear < startTime.getFullYear() && startDate.getMonth() < startTime.getMonth()&&startDate.getDate() < startTime.getDate()) || startDate.getHours() < startTime.getHours()){
+            return {
+                error: true,
+                message: "Elija una fecha posterior"
+            }
+        };
+        if(startDate.getFullYear > endDate.getFullYear() || (startDate.getFullYear > endDate.getFullYear() && startDate.getMonth() > endDate.getMonth()) || (startDate.getFullYear > endDate.getFullYear() && startDate.getMonth() > endDate.getMonth() && startDate.getDate() > endDate.getDate) || startDate.getHours() > endTime.getHours()){
+            return {
+                error: true,
+                message: "Elija una fecha anterior"
+            }
+        }
+        if(startTime.getHours()>field.closeHour || startTime.getHours()<field.openHour){
+            return {
+                error: true,
+                message: "Elija una hora donde la cancha esté abierta"
+            }
+        }
+        //Validacion por si se elije una fecha con reserva
+        return {
+            error: false,
+            message: ""
+        }
+    }
+
+    const handleSubmit = (e)=>{
+        e.preventDefault();
+        console.log(startDate);
+        onHide();
+        setStartDate(null);
+    }
+    
+    useEffect(()=>{
+        setErrorFecha(validacionFecha);
+    },[startDate]);
 
     return (
         <>
@@ -54,22 +102,30 @@ const Reservation = ({show, onHide, field}) => {
                     </Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
-                    <Form>
+                    <Form onSubmit={handleSubmit}>
                         <h3 className="text-muted mb-3">Usuario: {user.username}</h3>
                         <div className="d-flex flex-column flex-md-row my-3">
-                            <Form.Group className="reservation-input-size d-flex flex-column mb-4 mb-md-0">
-                                <Form.Label className="fw-medium">Nombre: </Form.Label>
-                                <Form.Control type="text" placeholder="Ingresa tu Nombre"/>
-                            </Form.Group>
-                            <Form.Group className="d-flex flex-column reservation-input-size mb-4 mb-md-0">
+                            <Form.Group className="d-flex flex-column w-100">
                                 <Form.Label className="fw-medium">Seleccione una fecha: </Form.Label>  {/*FALTA EL LIMITE INFERIOR DE HS Y FALTA EL EXCLUDE POR RESERVAS HECHAS*/}
-                                <DatePicker showIcon showTimeSelect minDate={new Date()} maxDate={endDate} filterTime={filterReservedHours} className="comment-border" timeCaption="Hora" minTime={startTime} maxTime={endTime} placeholderText="Click aquí" timeFormat="HH:mm" timeIntervals={60} dateFormat="dd/MM/yyyy h aa" selected={startDate} onChange={(date) => setStartDate(date)} />
-                            </Form.Group> {/*filterDate={}  excludeDates={[]} excludeTimes={[]}*/ }
+                                <DatePicker showIcon showTimeSelect minDate={new Date()} maxDate={endDate} timeCaption="Hora" minTime={startTime} maxTime={endTime} placeholderText="Click aquí" timeFormat="HH:mm" timeIntervals={60} dateFormat="dd/MM/yyyy h aa" selected={startDate} onChange={(date) => {setStartDate(date); setClicked(true)}} required className={`comment-border ${clsx(
+                                    "form-control", 
+                                    {
+                                        "is-invalid": errorFecha.error && clicked,
+                                    },
+                                    {
+                                        "is-valid": !errorFecha.error && clicked,
+                                    }
+                                )}`}/>
+                                {errorFecha.error && clicked &&(
+                                    <div className="text-danger fw-bolder my-2">
+                                        <span rol="alert">{errorFecha.message}</span>
+                                    </div>
+                                )}
+                            </Form.Group> {/*filterDate={} filterTime={filterReservedHours} excludeDates={[]} excludeTimes={[]}*/ }
                         </div>
                         <Form.Group className="d-flex flex-column flex-md-row justify-content-md-end gap-3">
-                            <Button variant="outline-danger" className="btn-reservation" type="button" onClick={onHide}>Cerrar</Button>
-                            <Button variant="outline-success" className="btn-reservation" type="submit" onClick={()=>{
-                            }}>Reservar</Button>
+                            <Button variant="outline-danger" className="btn-reservation" type="button"  onClick={()=>{onHide(); setStartDate(null);}}>Cerrar</Button>
+                            <Button variant="outline-success" className="btn-reservation" type="submit">Reservar</Button>
                         </Form.Group>
                     </Form>
                     <p className="text-muted my-2">Podes alquilar tu cancha entre 1 o 60 dias antes **</p>
