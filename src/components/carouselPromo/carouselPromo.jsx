df85682d70a02cf3fcd5681e862896c658c9cd05
Pablo Matias Rodriguez <Pablomatiasrodriguez95@gmail.com>
2023-08-21 10:49:00 -0300

    Modificando Banner para promos de Messi

--- src/components/carouselPromo/carouselPromo.jsx
+++ src/components/carouselPromo/carouselPromo.jsx
@@ -1,5 +1,5 @@
 import Carousel from 'react-bootstrap/Carousel';
-
+import { Link } from 'react-router-dom';
 import sliderImage1 from './imgCarouselPromo/slider1.jpg';
 import sliderImage2 from './imgCarouselPromo/slider2.jpg';
 import sliderImage3 from './imgCarouselPromo/slider3.jpg';
@@ -8,13 +8,19 @@ const Slider = () => {
   return (
     <Carousel>
       <Carousel.Item>
-        <img className="d-block w-100 carousel-image" src={sliderImage1} alt="First slide" />
+        <a href="https://www.pepsi.com/" target="_blank" rel="noopener noreferrer">
+          <img className="d-block w-100 carousel-image" src={sliderImage1} alt="First slide" />
+        </a>
       </Carousel.Item>
       <Carousel.Item>
-        <img className="d-block w-100 carousel-image" src={sliderImage2} alt="First slide" />
+        <a href="https://www.ypf.com/Paginas/home.aspx" target="_blank" rel="noopener noreferrer">
+          <img className="d-block w-100 carousel-image" src={sliderImage2} alt="First slide" />
+        </a>
       </Carousel.Item>
       <Carousel.Item>
-        <img className="d-block w-100 carousel-image" src={sliderImage3} alt="First slide" />
+        <a href="https://lays.com/" target="_blank" rel="noopener noreferrer">
+          <img className="d-block w-100 carousel-image" src={sliderImage3} alt="First slide" />
+        </a>
       </Carousel.Item>
     </Carousel>
   );
