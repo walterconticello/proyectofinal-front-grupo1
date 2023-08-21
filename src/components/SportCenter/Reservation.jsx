b3389bec38481f5f6d7d8c0e042a8d510d07ac13
Diego Vaca Paz <diegovacapaz@gmail.com>
2023-08-21 14:52:38 -0300

    Se instalo date picker

--- src/components/SportCenter/Reservation.jsx
+++ src/components/SportCenter/Reservation.jsx
@@ -1,4 +1,4 @@
-import { Button, Modal } from "react-bootstrap";
+import { Button, Form, Modal } from "react-bootstrap";
 
 const Reservation = ({show, onHide, field}) => {
     return (
@@ -10,12 +10,29 @@ const Reservation = ({show, onHide, field}) => {
                     </Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
-                    <p className="text-muted">Podes alquilar tu cancha entre 1 o 60 dias antes **</p>
+                <Form>
+                        <Form.Group className="mb-3" controlId="formBasicEmail">
+                            <Form.Label>Email address</Form.Label>
+                            <Form.Control type="email" placeholder="Enter email" />
+                            <Form.Text className="text-muted">
+                                We'll never share your email with anyone else.
+                            </Form.Text>
+                        </Form.Group>
+
+                        <Form.Group className="mb-3" controlId="formBasicPassword">
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control type="password" placeholder="Password" />
+                        </Form.Group>
+                        <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                            <Form.Check type="checkbox" label="Check me out" />
+                        </Form.Group>
+                        <Form.Group className="d-flex flex-column flex-md-row justify-content-md-end gap-3">
+                            <Button variant="outline-danger" className="btn-reservation" type="button" onClick={onHide}>Cerrar</Button>
+                            <Button variant="outline-success" className="btn-reservation" type="submit">Reservar</Button>
+                        </Form.Group>
+                    </Form>
+                    <p className="text-muted my-2">Podes alquilar tu cancha entre 1 o 60 dias antes **</p>
                 </Modal.Body>
-                <Modal.Footer>
-                    <Button variant="outline-danger" className="btn-reservation" onClick={onHide}>Cerrar</Button>
-                    <Button variant="outline-success" className="btn-reservation">Reservar</Button>
-                </Modal.Footer>
             </Modal>
         </>
     );
