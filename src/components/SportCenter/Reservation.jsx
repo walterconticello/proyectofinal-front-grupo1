652757612496e1713d8fdc926eb740136bb1e1de
Diego Vaca Paz <diegovacapaz@gmail.com>
2023-08-21 23:54:32 -0300

    Se hizo el minimo de 1 dia despues

--- src/components/SportCenter/Reservation.jsx
+++ src/components/SportCenter/Reservation.jsx
@@ -22,14 +22,53 @@ const Reservation = ({show, onHide, field}) => {
         if(month + offset <= 11){
             return month + offset;
         }
-        return (month + offset - 11);
+        return (month + offset - 11) - 1;
+    }
+
+    const addDay = (day, month,offset) => {
+        switch(month){
+            case 0: //31 days
+            case 2:
+            case 4:
+            case 6:
+            case 7:
+            case 9:
+            case 11:
+                if(day + offset <= 31){
+                    return day + offset;
+                }
+                return (day + offset - 31);
+                break;
+            case 3: //30 days
+            case 5:
+            case 8:
+            case 10:
+                if(day + offset <= 30){
+                    return day + offset;
+                }
+                return (day + offset - 30);
+                break;
+            case 1:
+                if(day + offset <= 28){
+                    return day + offset;
+                }
+                return (day + offset - 28);
+                break;
+            default:
+                break;
+        }
+        
+        if(day + offset <=23){
+            return day + offset;
+        }
+        return (day + offset - 23) - 1;
     }
 
     const URL = import.meta.env.VITE_DB;
     const endDate = new Date(new Date().getFullYear(), addMonth(new Date().getMonth(), 2), new Date().getDate());
     const startTime = new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDate(),field.openHour);
     const endTime = new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDate(),field.closeHour - 1);
-    
+
     const fetchReservation = async () => { //Esto deberia filtrar por id de field, pero el dbjson no puedo por eso lo filtro en el try-catch
         try {
             const response = await fetch(`${URL}reservations`);
@@ -149,7 +188,7 @@ const Reservation = ({show, onHide, field}) => {
                         <div className="d-flex flex-column flex-md-row my-3">
                             <Form.Group className="d-flex flex-column w-100">
                                 <Form.Label className="fw-medium">Seleccione una fecha: </Form.Label>  {/*FALTA EL LIMITE INFERIOR DE HS Y FALTA EL EXCLUDE POR RESERVAS HECHAS*/}
-                                <DatePicker showIcon showTimeSelect minDate={new Date()} maxDate={endDate} excludeTimes={filterReservedHours()} timeCaption="Hora" minTime={startTime} maxTime={endTime} placeholderText="Click aquí" timeFormat="HH:mm" timeIntervals={60} dateFormat="dd/MM/yyyy h aa" selected={startDate} onChange={(date) => {setStartDate(date); setClicked(true)}} required className={`comment-border ${clsx(
+                                <DatePicker showIcon showTimeSelect minDate={(new Date()).setDate(addDay((new Date()).getDate(),(new Date()).getMonth(),1))} maxDate={endDate} excludeTimes={filterReservedHours()} timeCaption="Hora" minTime={startTime} maxTime={endTime} placeholderText="Click aquí" timeFormat="HH:mm" timeIntervals={60} dateFormat="dd/MM/yyyy h aa" selected={startDate} onChange={(date) => {setStartDate(date); setClicked(true)}} required className={`comment-border ${clsx(
                                     "form-control", 
                                     {
                                         "is-invalid": errorFecha.error && clicked,
