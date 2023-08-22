5b561733ff47ad992f703eb75948604a9e513d69
Pablo Matias Rodriguez <Pablomatiasrodriguez95@gmail.com>
2023-08-21 22:50:51 -0300

    Terminando detalles en Cards

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -29,7 +29,7 @@ const AboutUs = () => {
     {
      name: "Paul Walker" ,
       position: "Desarrollador" ,
-      description: "Descripción del desarrollador",
+      description: "lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
       imageSrc: "/TeamImg/PaulWalker.jpg",
       imageAlt: "Foto del desarrollador",
     },
