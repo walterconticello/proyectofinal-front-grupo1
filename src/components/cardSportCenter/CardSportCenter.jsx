4c467be8f84e7662d58ca82fda9732178e8dbfb4
Pablo Matias Rodriguez <Pablomatiasrodriguez95@gmail.com>
2023-08-23 15:04:58 -0300

    Modicando estilos de las cards

--- src/components/cardSportCenter/CardSportCenter.jsx
+++ src/components/cardSportCenter/CardSportCenter.jsx
@@ -11,17 +11,13 @@ const CardSportCenter = () => {
       { cards === undefined ? <h1>Cargando...</h1> : cards.map((card) =>(
       <Card key={card.id} className="h-80 card-custom">
         <div className="card-image-container">
-        <Card.Img src={card.image?.url} alt={card.name} className="card-image img-fluid" />
         </div>
         <Card.Body>
-          <Card.Title>{card.name}</Card.Title>
+          <Card.Title className='card-title'>{card.name}</Card.Title>
           <Card.Text>{card.description}</Card.Text>
           <div className="card-buttons d-flex justify-content-between">
-            <Button variant="primary" className="buy-button btn">
+            <Button className="button-alquilar">
               Alquilar
-            </Button>
-            <Button variant="secondary" className="details-button btn">
-              Details
             </Button>
           </div>
         </Card.Body>
