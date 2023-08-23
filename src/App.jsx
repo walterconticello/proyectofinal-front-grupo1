3653ae3b888d1e1cc1f5af0d1856cc046fdaa237
Lautaro Cruz Srur <lautaro.cruzsrur@gmail.com>
2023-08-23 11:36:42 -0300

    mergeando

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Routes from './routes/Router';     
+import FieldContext from './context/FieldContext.jsx';
+import CenterContext from './context/CenterContext';
+import ReserveContext from './context/ReservationContext';
+import NavBar from "./components/Navbar/Navbar"
+import Footer from "./components/Footer/Footer"
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <> 
+      <ReserveContext>
+    <CenterContext>
+      <FieldContext>
+        <NavBar/>
+      <Routes />
+      <Footer/>
+      </FieldContext>
+      </CenterContext>
+      </ReserveContext>
+
     </>
   ) 
 }
