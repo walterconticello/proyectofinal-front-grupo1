d2b94bf8ca63723c3c047814fde7b7ee3866bb20
mwarcos <mmwarcos@gmail.com>
2023-08-24 09:00:18 -0300

    404 not found page

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -6,18 +6,25 @@ import ProductDetails from "../pages/store/productDetails/ProductDetails";
 import Login from "../pages/Login/Login";
 import UserProfile from "../pages/UserProfile/UserProfile";
 import PrivateRoute from "./PrivateRoute";
+import Error404 from "../pages/Error404/Error404";
 
 const Routes = () => {
-
   return (
     <Rutas>
       <Route path="/" element={<Home />} />
       <Route path="/store" element={<Store />} />
       <Route path="/complejos" element={<List />} />
-      <Route path="/store/product/:productId"element={<ProductDetails />}
+      <Route path="/store/product/:productId" element={<ProductDetails />} />
+      <Route path="/login" element={<Login />} />
+      <Route
+        path="/perfil"
+        element={
+          <PrivateRoute>
+            <UserProfile />
+          </PrivateRoute>
+        }
       />
-      <Route path='/login' element={<Login />} />
-      <Route path='/perfil' element={<PrivateRoute><UserProfile /></PrivateRoute>} />
+      <Route path="/*" element={<Error404 />} />
     </Rutas>
   );
 };
