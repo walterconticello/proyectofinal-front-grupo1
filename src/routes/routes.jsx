5a1b504c4e053b67ac7ecfb3db9a3dd43f015304
mwarcos <mmwarcos@gmail.com>
2023-08-24 16:58:06 -0300

    test sportcenter

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -6,18 +6,23 @@ import ProductDetails from "../pages/store/productDetails/ProductDetails";
 import Login from "../pages/Login/Login";
 import UserProfile from "../pages/UserProfile/UserProfile";
 import PrivateRoute from "./PrivateRoute";
-
+import SportCenterDetail from "../components/SportCenter/SportCenterDetail";
 const Routes = () => {
-
   return (
     <Rutas>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<SportCenterDetail />} />
       <Route path="/store" element={<Store />} />
       <Route path="/complejos" element={<List />} />
-      <Route path="/store/product/:productId"element={<ProductDetails />}
+      <Route path="/store/product/:productId" element={<ProductDetails />} />
+      <Route path="/login" element={<Login />} />
+      <Route
+        path="/perfil"
+        element={
+          <PrivateRoute>
+            <UserProfile />
+          </PrivateRoute>
+        }
       />
-      <Route path='/login' element={<Login />} />
-      <Route path='/perfil' element={<PrivateRoute><UserProfile /></PrivateRoute>} />
     </Rutas>
   );
 };
