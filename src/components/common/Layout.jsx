d6f61b8dc7ca7da4d64429b6cefddd87ffa2065d
Walter Conticello <walterconticellomsmt@gmail.com>
2023-09-19 11:42:53 -0300

    bugfix menor

--- src/components/common/Layout.jsx
+++ src/components/common/Layout.jsx
@@ -1,9 +1,17 @@
+import { useContext, useEffect } from "react";
 import Footer from "./Footer/Footer"
 import Navbar from "./Navbar/Navbar"
+import { AuthContext } from "../../context/AuthContext";
 
 
 // eslint-disable-next-line react/prop-types
-const Layout = ({children}) => {
+const Layout = ({ children }) => {
+	const { authenticated, loading, getAuth } = useContext(AuthContext);
+
+	useEffect(() => {
+		getAuth();
+	}, [])
+
 	return (
 		<>
 			<Navbar />
