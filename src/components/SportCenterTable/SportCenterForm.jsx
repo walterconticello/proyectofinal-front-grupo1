ad256db9f63d5c9f39bd5fe19f070af74ff6aa4d
mwarcos <mmwarcos@gmail.com>
2023-09-24 18:31:42 -0300

    form de añadir sportcenter

--- src/components/SportCenterTable/SportCenterForm.jsx
+++ src/components/SportCenterTable/SportCenterForm.jsx
@@ -1,17 +1,8 @@
-import { useState, useContext, useEffect } from "react";
+import React, { useState, useContext } from "react";
 import { Formik, Form, Field, ErrorMessage } from "formik";
-
 import { CenterContext } from "../../context/CenterContext";
-import {
-  FaFutbol,
-  FaMapMarkerAlt,
-  FaCarAlt,
-  FaShower,
-  FaGlassCheers,
-  FaHouzz,
-  FaRestroom,
-  FaSistrix,
-} from "react-icons/fa";
+
+import * as Yup from "yup";
 import {
   Container,
   Row,
@@ -21,7 +12,7 @@ import {
   Button,
 } from "react-bootstrap";
 import { MdAddCircle } from "react-icons/md";
-import * as Yup from "yup";
+import { FaShower, FaHouzz, FaRestroom, FaGlassCheers } from "react-icons/fa";
 
 const validationSchema = Yup.object().shape({
   name: Yup.string()
@@ -31,21 +22,21 @@ const validationSchema = Yup.object().shape({
     .required("Required")
     .min(3, "La dirección debe tener al menos 3 caracteres")
     .max(30, "La dirección debe tener como máximo 30 caracteres"),
-  phone: Yup.number().required("Required"),
+  phone: Yup.string().required("Required"),
   services: Yup.object().shape({
     bar: Yup.boolean(),
     showers: Yup.boolean(),
-    Grill: Yup.boolean(),
+    grill: Yup.boolean(),
     parking: Yup.boolean(),
   }),
   social: Yup.object().shape({
     facebook: Yup.string().min(3, "Debe tener al menos 3 caracteres"),
     instagram: Yup.string().min(3, "Debe tener al menos 3 caracteres"),
   }),
-  latitude: Yup.number(),
-  location: Yup.string()
-    .min(3, "Debe tener al menos 3 caracteres")
-    .max(30, "Debe tener como máximo 30 caracteres"),
+  location: Yup.object().shape({
+    latitude: Yup.number(),
+    longitude: Yup.number(),
+  }),
 });
 
 const SportCenterForm = () => {
@@ -60,19 +51,21 @@ const SportCenterForm = () => {
     address: "",
     phone: "",
     services: {
-      bar: "",
-      showers: "",
-      grill: "",
-      parking: "",
+      bar: false,
+      showers: false,
+      grill: false,
+      parking: false,
     },
-    fields: "",
-    photo: "",
     social: {
       facebook: "",
       instagram: "",
     },
-    location: "",
+    location: {
+      latitude: "",
+      longitude: "",
+    },
   };
+
   return (
     <Container>
       <Button
@@ -109,7 +102,12 @@ const SportCenterForm = () => {
                   <Col md={12} lg={6}>
                     <BootstrapForm.Group>
                       <label className="form-label">Nombre</label>
-                      <Field type="text" name="name" className="form-control" />
+                      <Field
+                        type="text"
+                        name="name"
+                        placeholder="Mi complejo"
+                        className="form-control"
+                      />
                       <ErrorMessage
                         name="name"
                         component="div"
@@ -118,10 +116,11 @@ const SportCenterForm = () => {
                     </BootstrapForm.Group>
 
                     <BootstrapForm.Group>
-                      <label className="form-label">Direccion</label>
+                      <label className="form-label">Dirección</label>
                       <Field
                         type="text"
                         name="address"
+                        placeholder="Av. Siempre Viva 123"
                         className="form-control"
                       />
                       <ErrorMessage
@@ -132,10 +131,11 @@ const SportCenterForm = () => {
                     </BootstrapForm.Group>
 
                     <BootstrapForm.Group>
-                      <label className="form-label">Telefono</label>
+                      <label className="form-label">Teléfono</label>
                       <Field
                         type="text"
                         name="phone"
+                        placeholder="+54 9 381 000 1110"
                         className="form-control"
                       />
                       <ErrorMessage
@@ -144,68 +144,98 @@ const SportCenterForm = () => {
                         className="text-danger"
                       />
                     </BootstrapForm.Group>
+
+                    <BootstrapForm.Group>
+                      <label className="form-label">Facebook</label>
+                      <Field
+                        type="text"
+                        name="social.facebook"
+                        placeholder="https://www.facebook.com/"
+                        className="form-control"
+                      />
+                      <label className="form-label">Instagram</label>
+                      <Field
+                        type="text"
+                        name="social.instagram"
+                        placeholder="https://www.instagram.com/"
+                        className="form-control"
+                      />
+                    </BootstrapForm.Group>
                   </Col>
                   <Col md={12} lg={6}>
                     <BootstrapForm.Group>
-                      <label className="form-label">Services:</label>
-                      <div className="d-flex flex-wrap align-items-center justify-content-start">
-                        <label className="form-check d-flex">
-                          <Field
-                            type="checkbox"
-                            name="services.showers"
-                            className="form-label"
-                          />
-                          <FaShower size={23} /> Duchas
-                        </label>
-                        <label className="form-check d-flex ">
-                          <Field
-                            type="checkbox"
-                            name="services.Grill"
-                            className="form-label"
-                          />
-                          <FaHouzz size={23} /> Parrillas
-                        </label>
-                        <label className="form-check d-flex">
-                          <Field
-                            type="checkbox"
-                            name="services.dressingRoom"
-                            className="form-label"
-                          />
-                          <FaRestroom size={23} /> Vestuario
-                        </label>
-                        <label className="form-check d-flex">
-                          <Field
-                            type="checkbox"
-                            name="services.bar"
-                            className="form-label"
-                          />
-                          <FaGlassCheers size={23} /> Bar
-                        </label>
-                      </div>
+                      <label className="form-label">Servicios</label>
+                      <Row className="mb-3 d-flex ">
+                        <Col md={6}>
+                          <label className="form-check d-flex">
+                            <Field
+                              type="checkbox"
+                              name="services.showers"
+                              className="form-label"
+                            />
+                            <FaShower size={23} /> Duchas
+                          </label>
+                          <label className="form-check d-flex ">
+                            <Field
+                              type="checkbox"
+                              name="services.Grill"
+                              className="form-label"
+                            />
+                            <FaHouzz size={23} /> Parrillas
+                          </label>
+                        </Col>
+                        <Col md={6}>
+                          <label className="form-check d-flex">
+                            <Field
+                              type="checkbox"
+                              name="services.dressingRoom"
+                              className="form-label"
+                            />
+                            <FaRestroom size={23} /> Vestuario
+                          </label>
+                          <label className="form-check d-flex">
+                            <Field
+                              type="checkbox"
+                              name="services.bar"
+                              className="form-label"
+                            />
+                            <FaGlassCheers size={23} /> Bar
+                          </label>
+                        </Col>
+                      </Row>
                       <ErrorMessage
                         name="services"
                         component="div"
                         className="text-danger"
                       />
                     </BootstrapForm.Group>
+
                     <BootstrapForm.Group>
-                      <label className="form-label">Imagen</label>
-                      <input
-                        type="file"
-                        name="photo"
+                      <label className="form-label">Latitud</label>
+                      <Field
+                        type="text"
+                        name="location.latitude"
                         className="form-control"
-                        onChange={(e) =>
-                          setFieldValue("image", e.target.files[0])
-                        }
+                        placeholder="Latitud"
                       />
-                      <ErrorMessage
-                        name="photo"
-                        component="div"
-                        className="text-danger"
+                    </BootstrapForm.Group>
+
+                    <BootstrapForm.Group>
+                      <label className="form-label">Longitud</label>
+                      <Field
+                        type="text"
+                        name="location.longitude"
+                        className="form-control"
+                        placeholder="Longitud"
                       />
                     </BootstrapForm.Group>
                   </Col>
                 </Row>
+                <div className="btn-container d-flex justify-content-end">
+                  <Button type="submit" className="btn add-button px-5">
+                    Guardar
+                  </Button>
+                </div>
               </Form>
             )}
           </Formik>
