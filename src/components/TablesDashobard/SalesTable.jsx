044ae65eb71f521ad82b1f561d570a4a71f56c8c
mwarcos <mmwarcos@gmail.com>
2023-10-09 23:27:34 -0300

    corregi estilos de las tablas del dashboard

--- src/components/TablesDashobard/SalesTable.jsx
+++ src/components/TablesDashobard/SalesTable.jsx
@@ -6,36 +6,39 @@ import { AuthContext } from "../../context/AuthContext";
 const SalesTable = ({ sales }) => {
   const { getProduct } = useContext(ProductContext);
   const { users } = useContext(AuthContext);
-  const limitedSales = sales.slice(0, 5);
 
   const formatDate = (dateString) => {
     const options = { year: "numeric", month: "2-digit", day: "2-digit" };
     return new Date(dateString).toLocaleDateString(undefined, options);
   };
-  // console.log(sales);
   return (
-    <Table responsive striped="columns">
-      <thead>
-        <tr>
-          <th>Nombre</th>
-          <th>Usuario</th>
-          <th>Cantidad</th>
-          <th>Total</th>
-          <th>Fecha</th>
-        </tr>
-      </thead>
-      <tbody>
-        {limitedSales.map((sale, index) => (
-          <tr key={sale._id}>
-            <td>{sale.productId.name}</td>
-            <td>{sale.userId.username}</td>
-            <td>{sale.quantity}</td>
-            <td>{sale.totalPrice}</td>
-            <td>{formatDate(sale.date)}</td>
+    <div
+      className="table-container"
+      style={{ maxHeight: "400px", overflowY: "auto" }}
+    >
+      <Table responsive striped="columns">
+        <thead>
+          <tr>
+            <th>Nombre</th>
+            <th>Usuario</th>
+            <th>Cantidad</th>
+            <th>Total</th>
+            <th>Fecha</th>
           </tr>
-        ))}
-      </tbody>
-    </Table>
+        </thead>
+        <tbody>
+          {sales.map((sale, index) => (
+            <tr key={sale._id}>
+              <td>{sale.productId.name}</td>
+              <td>{sale.userId.username}</td>
+              <td>{sale.quantity}</td>
+              <td>{sale.totalPrice}</td>
+              <td>{formatDate(sale.date)}</td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+    </div>
   );
 };
 
