5a4f27490fcf471b07db19a71093629260502362
mwarcos <mmwarcos@gmail.com>
2023-10-28 15:11:24 -0300

    [hotFix-Contacto] integre emailjs en el contact form. añadi un toastify indicando que el mail fue enviado correctamente

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -8,27 +8,29 @@ import "react-toastify/dist/ReactToastify.css";
 
 const ContactForm = () => {
   const handleSubmit = (values, { resetForm }) => {
+    sendEmail(values);
     resetForm();
   };
 
   const validationSchema = Yup.object().shape({
-    name: Yup.string()
+    user_name: Yup.string()
       .matches(/^[a-zA-Z\s]+$/, "El nombre solo debe tener letras y espacios")
       .required("El nombre es requerido"),
-    email: Yup.string().email("Email invalido").required("Email es requerido"),
+    user_email: Yup.string()
+      .email("Email inválido")
+      .required("Email es requerido"),
     message: Yup.string()
-      .required("Message is required")
-      .test("characterLimit", "Maximo 200 caracteres", (value) => {
+      .required("El mensaje es requerido")
+      .test("Máximo 200 caracteres", (value) => {
         return !value || value.length <= 200;
       }),
   });
 
   const [messageLength, setMessageLength] = useState(0);
   const form = useRef();
-
-  const sendEmail = (e) => {
-    e.preventDefault();
-
+  const sendEmail = (values) => {
+    console.log(values);
+    console.log(form.current);
     emailjs
       .sendForm(
         "service_l98j15g",
@@ -36,9 +38,11 @@ const ContactForm = () => {
         form.current,
         "AEYEb1Mx0uSTLvMxZ"
       )
-      .then((result) => {})
+      .then((result) => {
+        toast.success("Mensaje enviado con éxito");
+      })
       .catch((error) => {
-        console.log(error.text);
+        toast.error("Error al enviar el formulario. :(");
       });
   };
 
@@ -51,28 +55,33 @@ const ContactForm = () => {
         </p>
       </div>
       <Formik
-        initialValues={{ name: "", email: "", message: "" }}
+        initialValues={{ user_name: "", user_email: "", message: "" }}
         validationSchema={validationSchema}
         onSubmit={handleSubmit}
       >
         {({ touched, errors, values, setFieldValue }) => (
-          <Form ref={form} onSubmit={sendEmail} className="my-3">
+          <Form ref={form} className="my-3">
             <div className="form-group w-100">
               <label htmlFor="name" className="label">
                 Name:
               </label>
-              <Field type="text" id="name" name="name" className="input" />
-              {touched.name && errors.name && (
-                <div className="error-message">{errors.name}</div>
+              <Field type="text" id="name" name="user_name" className="input" />
+              {touched.user_name && errors.user_name && (
+                <div className="error-message">{errors.user_name}</div>
               )}
             </div>
             <div className="form-group w-100">
               <label htmlFor="email" className="label">
                 Email:
               </label>
-              <Field type="email" id="email" name="email" className="input" />
-              {touched.email && errors.email && (
-                <div className="error-message">{errors.email}</div>
+              <Field
+                type="email"
+                id="email"
+                name="user_email"
+                className="input"
+              />
+              {touched.user_email && errors.user_email && (
+                <div className="error-message">{errors.user_email}</div>
               )}
             </div>
             <div className="form-group w-100">
@@ -88,9 +97,12 @@ const ContactForm = () => {
                 onChange={(e) => {
                   const newValue = e.target.value;
                   setMessageLength(newValue.length);
-
-                  // Actualiza el valor del campo de mensaje en Formik
                   setFieldValue("message", newValue);
+                  if (newValue.length >= 200) {
+                    e.target.value = newValue.slice(0, 200);
+                    setMessageLength(200);
+                  }
+                  setFieldValue("message", e.target.value);
                 }}
               />
               <div className="character-count text-end">
@@ -100,12 +112,13 @@ const ContactForm = () => {
                 <div className="error-message">{errors.message}</div>
               )}
             </div>
-            <button type="submit" className="submit-button" value="Send">
+            <button type="submit" className="submit-button">
               Submit
             </button>
           </Form>
         )}
       </Formik>
+      <ToastContainer />
     </div>
   );
 };
